ac4d103459f368974090c054b38f6aee171606a1
Vens <v.kolev@alfatrex.com>
2023-07-27 09:23:17 +0200

    Add first component Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+// import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// @Component({
+//   imports: [
+//     HomeComponent,
+//   ],
+//   template: `
+//   <main>
+//     <header class="brand-name">
+//       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+//     </header>
+//     <section class="content">
+//       <app-home></app-home>
+//     </section>
+//   </main>
+// `,
+//   })
 export class AppComponent {
   title = 'Hello in main project';
 }
